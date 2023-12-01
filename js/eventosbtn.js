66ddd8a610d4d00e7c171a94ad3672741d616f87
Brenda <mourabr907@gmail.com>
2023-11-30 23:54:05 -0300

    deixei algumas telas padroes, implementei evento para os botoes

---
+++ js/eventosbtn.js
@@ -0,0 +1,9 @@
+// Função para alternar a marcação do botão de rádio
+function alternarMarcacaoRadio(){
+    var radio = document.getElementById('opcao');
+    
+    // Se o botão de rádio estiver marcado, desmarque-o; caso contrário, marque-o
+    radio.checked = !radio.checked;
+  }
+  
+  
